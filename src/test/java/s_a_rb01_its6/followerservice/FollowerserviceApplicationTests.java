1fe252423948150618bcf627d5f5c672d90b89ec
Björn Wildeman <bwildeman99@gmail.com>
2025-01-08 17:49:39 +0100

    test properties

--- src/test/java/s_a_rb01_its6/followerservice/FollowerserviceApplicationTests.java
+++ src/test/java/s_a_rb01_its6/followerservice/FollowerserviceApplicationTests.java
@@ -2,7 +2,9 @@ package s_a_rb01_its6.followerservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class FollowerserviceApplicationTests {
 
